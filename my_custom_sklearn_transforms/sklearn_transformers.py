c8d17625b12e2573592f5b87db8741d0ae4e92c7
metr01ds <70035405+metr01ds@users.noreply.github.com>
2020-08-25 12:32:48 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,20 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+#localiza valores maiores que 10 na coluna especificada e substitui por "10"
+class CorrigeNotas(BaseEstimator, TransformerMixin):
+    def __init__(self, dados, columns, val_max, valor):
+        self.dados = dados
+        self.columns = columns
+        self.val_max = val_max
+        self.valor = valor
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        self.dados[self.columns] = self.dados[self.columns].where(self.dados[self.columns]<=self.val_max,other=self.valor, axis=1)
+        return self.dados
